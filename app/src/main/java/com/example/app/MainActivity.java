0c0e4041345bfa98ed499e5d32a1253d55d05464
MatiasDesu <comtusprogramas@gmail.com>
2024-03-30 09:54:29 -0300

    Se implemento la funcionalidad para enviar archivos (imprimirlos), se elimino la imagen que estaba por defecto en la pantalla principal y se cambio por un texto. Se elimino el archivo spinner_layout que no tenia uso.

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -13,19 +13,32 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.net.Socket;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private ImageView imageView;
+    private TextView textView;
     private static final int REQUEST_CODE_OPEN_FILE = 123;
+    private Uri uri;
+    private String serverName;
+    private int serverPort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.textView);
     }
 
     @Override
@@ -100,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 // Obtener las opciones seleccionadas
                 String seleccionColor = (String) colorSpinner.getSelectedItem();
                 String seleccionTamano = (String) tamanoSpinner.getSelectedItem();
+                enviarDatosImpresion(uri, seleccionColor, seleccionTamano);
                 Toast.makeText(MainActivity.this, "Imprimiendo, Color: " + seleccionColor + ", Tamaño: " + seleccionTamano, Toast.LENGTH_LONG).show();
             }
         });
@@ -108,16 +123,10 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-
     private void abrirArchivo() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*"); // Selecciona todos los tipos de archivo por defecto
-
-        // Agrega los tipos MIME que deseas aceptar
-        String[] mimeTypes = {"application/pdf", "image/*", "application/msword", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"};
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-
         startActivityForResult(intent, REQUEST_CODE_OPEN_FILE);
     }
 
@@ -126,17 +135,57 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_OPEN_FILE && resultCode == RESULT_OK) {
             if (data != null) {
-                Uri uri = data.getData();
+                uri = data.getData();
                 try {
                     // Lee la imagen seleccionada desde su URI
                     InputStream inputStream = getContentResolver().openInputStream(uri);
                     Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
                     // Muestra la imagen en el ImageView
                     imageView.setImageBitmap(bitmap);
+                    textView.setVisibility(View.GONE);
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    private void enviarDatosImpresion(Uri imageUri, String color, String size) {
+
+        serverName = "192.168.101.11";
+        serverPort = 12345;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Establecemos la conexión con el servidor en el puerto 12345
+                    Socket socket = new Socket(serverName, serverPort);
+
+                    // Creamos un flujo de salida de datos para enviar la imagen
+                    DataOutputStream dos = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+
+                    // Abrimos el archivo de imagen
+                    InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                    // Creamos un buffer de bytes para leer la imagen
+                    byte[] buffer = new byte[4096];
+
+                    // Variable para almacenar el número total de bytes leídos
+                    int bytesRead;
+
+                    // Leemos el archivo de imagen y lo enviamos al servidor
+                    while ((bytesRead = inputStream.read(buffer)) != -1) {
+                        dos.write(buffer, 0, bytesRead);
+                    }
+
+                    // Cerramos el flujo de salida de datos y el socket
+                    inputStream.close();
+                    dos.close();
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
